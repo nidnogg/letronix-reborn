dc9e559b8e4e0b5db97739df88a1d7af33a26297
nidnogg <henriquevt98@gmail.com>
2020-06-27 00:47:18 -0300

    proper initial commit by tloriato

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
+const charToImageMap = {
+  a: require("./assets/a.png"),
+  b: require("./assets/b.png"),
+  c: require("./assets/c.png"),
+  d: require("./assets/d.png"),
+  e: require("./assets/e.png"),
+  f: require("./assets/f.png")
+};
+
 export default function App() {
+  const [text, setText] = useState("");
+
+  const handleChange = ({ target }) => setText(target.value);
+
+  const updateLetronix = () => {
+    const arrayOfChars = [];
+
+    for (let i = 0; i < text.length; i++) {
+      arrayOfChars.push(text.charAt(i).toLocaleLowerCase());
+    }
+
+    return arrayOfChars.map(char => {
+      return <img src={charToImageMap[char]} alt="Letronix" />;
+    });
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <input value={text} onChange={handleChange} />
+      <div>{updateLetronix()}</div>
     </div>
   );
 }
