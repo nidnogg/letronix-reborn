b9225ecb574eeec7a87a086d0a1bea920123bb01
fkuhnert <fvk98@yahoo.com.br>
2020-06-27 19:42:42 -0300

    Broken require commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,63 @@
 import React, { useState } from "react";
 import "./styles.css";
 
-const charToImageMap = {
-  a: require("./assets/a.png"),
-  b: require("./assets/b.png"),
-  c: require("./assets/c.png"),
-  d: require("./assets/d.png"),
-  e: require("./assets/e.png"),
-  f: require("./assets/f.png"),
-  g: require("./assets/g.png"),
-  h: require("./assets/h.png"),
-  i: require("./assets/i.png"),
-  j: require("./assets/j.png"),
-  k: require("./assets/k.png"),
-  l: require("./assets/l.png"),
-  m: require("./assets/m.png"),
-  n: require("./assets/n.png"),
-  o: require("./assets/o.png"),
-  p: require("./assets/p.png"),
-  q: require("./assets/q.png"),
-  r: require("./assets/r.png"),
-  s: require("./assets/s.png"),
-  t: require("./assets/t.png"),
-  u: require("./assets/u.png"),
-  v: require("./assets/v.png"),
-  w: require("./assets/w.png"),
-  x: require("./assets/x.png"),
-  y: require("./assets/y.png"),
-  z: require("./assets/z.png"),
-  ' ': require("./assets/spc.png")
-};
+const alphabet = [
+  { char: 'a', variants: 1 },
+  { char: 'b', variants: 1 },
+  { char: 'c', variants: 1 },
+  { char: 'd', variants: 1 },
+  { char: 'e', variants: 1 },
+  { char: 'f', variants: 1 },
+  { char: 'g', variants: 1 },
+  { char: 'h', variants: 1 },
+  { char: 'i', variants: 1 },
+  { char: 'j', variants: 1 },
+  { char: 'k', variants: 1 },
+  { char: 'l', variants: 1 },
+  { char: 'm', variants: 1 },
+  { char: 'n', variants: 1 },
+  { char: 'o', variants: 1 },
+  { char: 'p', variants: 1 },
+  { char: 'q', variants: 1 },
+  { char: 'r', variants: 1 },
+  { char: 's', variants: 1 },
+  { char: 't', variants: 1 },
+  { char: 'u', variants: 1 },
+  { char: 'v', variants: 1 },
+  { char: 'w', variants: 1 },
+  { char: 'x', variants: 1 },
+  { char: 'y', variants: 1 },
+  { char: 'z', variants: 1 }
+];
 
+
+const Letter = props => {
+  { sourcePath, variant } = props;
+  return <img src={`sourcePath${variant}.png`}></img>
+}
+
+/* require sucks ass! fuck require */
+// https://stackoverflow.com/questions/30752365/node-js-require-path-stored-in-a-variable
+
+const charToImageMap = {};
+
+alphabet.map((letter) => {
+  charToImageMap[letter.char] = [];
+  for (let i = 0; i < letter.variants; i++) {
+    console.log(`./assets/${letter.char}.png`);
+    charToImageMap[letter.char].push(require(`./assets/${letter.char}.png`))
+  }
+});
 
 export default function App() {
   const [text, setText] = useState("");
 
   const handleChange = ({ target }) => setText(target.value);
+
+  const getLetterImage = (char) => {
+    return char;
+    // retornar a imagem dentro das variacoes
+  };
 
   const updateLetronix = () => {
     const arrayOfChars = [];
@@ -45,7 +67,8 @@ export default function App() {
     }
 
     return arrayOfChars.map(char => {
-      return <img className={char == char.toLowerCase() ? "lowercase" : "uppercase"} src={charToImageMap[char.toLocaleLowerCase()]} alt={char}/>;
+      console.log(charToImageMap);
+      return <img className={char == char.toLowerCase() ? "lowercase" : "uppercase"} src={charToImageMap[char]} alt={char} />;
     });
   };
 
@@ -53,7 +76,7 @@ export default function App() {
     <div className="App">
       <header>
         <h3>letronix reborn!</h3>
-        <p> 
+        <p>
           Digite texto abaixo e receba Letronix:
         </p>
       </header>
