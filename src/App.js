30556507b9ac4e1673c7c75663ba0d8ce2610938
nidnogg <henriquevt98@gmail.com>
2020-06-27 21:59:10 -0300

    bug almost solved, needs key fix

--- src/App.js
+++ src/App.js
@@ -1,40 +1,52 @@
 import React, { useState } from "react";
 import "./styles.css";
+import images from './assets/*.png';
+
+
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
+
+//const images = importAll(require.context('./assets', false, /\.(gif|jpe?g|svg)$/));
+//const images = require('./assets/*.png');
 
 const alphabet = [
-  { char: 'a', sourcePath:"./assets/a.png" },
-  { char: 'b', sourcePath:"./assets/b.png" },
-  { char: 'c', sourcePath:"./assets/c.png" },
-  { char: 'd', sourcePath:"./assets/d.png" },
-  { char: 'a', sourcePath:"./assets/a.png" },
-  { char: 'e', sourcePath:"./assets/e.png" },
-  { char: 'f', sourcePath:"./assets/f.png" },
-  { char: 'g', sourcePath:"./assets/g.png" },
-  { char: 'h', sourcePath:"./assets/h.png" },
-  { char: 'i', sourcePath:"./assets/i.png" },
-  { char: 'j', sourcePath:"./assets/j.png" },
-  { char: 'k', sourcePath:"./assets/k.png" },
-  { char: 'l', sourcePath:"./assets/l.png" },
-  { char: 'm', sourcePath:"./assets/m.png" },
-  { char: 'n', sourcePath:"./assets/n.png" },
-  { char: 'o', sourcePath:"./assets/o.png" },
-  { char: 'p', sourcePath:"./assets/p.png" },
-  { char: 'q', sourcePath:"./assets/q.png" },
-  { char: 'r', sourcePath:"./assets/r.png" },
-  { char: 's', sourcePath:"./assets/s.png" },
-  { char: 't', sourcePath:"./assets/t.png" },
-  { char: 'u', sourcePath:"./assets/u.png" },
-  { char: 'v', sourcePath:"./assets/v.png" },
-  { char: 'w', sourcePath:"./assets/w.png" },
-  { char: 'x', sourcePath:"./assets/x.png" },
-  { char: 'y', sourcePath:"./assets/y.png" },
-  { char: 'z', sourcePath:"./assets/z.png" }
+  { char: 'a', sourcePaths:["./assets/a.png"] },
+  { char: 'b', sourcePaths:["./assets/b.png"] },
+  { char: 'c', sourcePaths:["./assets/c.png", "./assets/c1.png"] },
+  { char: 'd', sourcePaths:["./assets/d.png"] },
+  { char: 'a', sourcePaths:["./assets/a.png"] },
+  { char: 'e', sourcePaths:["./assets/e.png"] },
+  { char: 'f', sourcePaths:["./assets/f.png"] },
+  { char: 'g', sourcePaths:["./assets/g.png"] },
+  { char: 'h', sourcePaths:["./assets/h.png"] },
+  { char: 'i', sourcePaths:["./assets/i.png"] },
+  { char: 'j', sourcePaths:["./assets/j.png"] },
+  { char: 'k', sourcePaths:["./assets/k.png"] },
+  { char: 'l', sourcePaths:["./assets/l.png"] },
+  { char: 'm', sourcePaths:["./assets/m.png"] },
+  { char: 'n', sourcePaths:["./assets/n.png"] },
+  { char: 'o', sourcePaths:["./assets/o.png"] },
+  { char: 'p', sourcePaths:["./assets/p.png"] },
+  { char: 'q', sourcePaths:["./assets/q.png"] },
+  { char: 'r', sourcePaths:["./assets/r.png"] },
+  { char: 's', sourcePaths:["./assets/s.png"] },
+  { char: 't', sourcePaths:["./assets/t.png"] },
+  { char: 'u', sourcePaths:["./assets/u.png"] },
+  { char: 'v', sourcePaths:["./assets/v.png"] },
+  { char: 'w', sourcePaths:["./assets/w.png"] },
+  { char: 'x', sourcePaths:["./assets/x.png"] },
+  { char: 'y', sourcePaths:["./assets/y.png"] },
+  { char: 'z', sourcePaths:["./assets/z.png"] }
 ];
 
 
 const Letter = props => {
-  const { sourcePath, variant } = props;
-  return <img src={sourcePath}></img>
+  const { letter } = props;
+  
+  return <img src={images['a.png']}></img>
 }
 
 /* require sucks ass! fuck require */
@@ -57,12 +69,36 @@ export default function App() {
 
   const getLetterImage = (char) => {
     return char;
-    // retornar a imagem dentro das variacoes
   };
 
-  const updateLetronix = () => {
-    const arrayOfChars = [];
 
+
+  const updateLetronix = () => {
+    const userInput = [];
+    for (let i = 0; i < text.length; i++) {
+      userInput.push(text.charAt(i));
+      console.log(images);
+    }
+  
+    const letters = userInput.map( (letter) => {
+      <img key={letter} src={images[key]}></img>
+    });
+
+    return userInput.map((char) => {
+      return <img key={char} className={char == char.toLowerCase() ? "lowercase" : "uppercase"} src={images[char]} alt={`${char}.png`} />;
+
+    });
+    /* works 
+    const file_name = 'a';
+
+    return (
+      <div>
+        <img src={images[file_name]} alt="nope.avi"/>
+      </div>
+    );
+
+    */
+    /*
     for (let i = 0; i < text.length; i++) {
       arrayOfChars.push(text.charAt(i));
     }
@@ -71,7 +107,8 @@ export default function App() {
       console.log(charToImageMap);
       return <img className={char == char.toLowerCase() ? "lowercase" : "uppercase"} src={charToImageMap[char]} alt={char} />;
     });
-  };
+    */
+  }
 
   return (
     <div className="App">
